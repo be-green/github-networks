d3322cd5f67c4f73fc2cd6148c003e949e978d02
be-green <brice.c.green@gmail.com>
2020-04-24 12:02:55 -0700

    update parsing logic to put into different tables

--- src/R/getTablesFromJSON.R
+++ src/R/getTablesFromJSON.R
@@ -1,5 +1,5 @@
 # method for compiling tables from JSON
-
+library(data.table)
 
 #' Adds a prefix to a data.table
 #' @param DT data.table to pass
@@ -7,12 +7,16 @@
 addPrefix <- function(DT, prefix) {
   setnames(DT,
            colnames(DT),
-           paste0("actor_", colnames(DT)))
+           paste0(prefix, colnames(DT)))
+  DT
 }
 
 getTable <- function(event, type) {
-  tbl <- as.data.table(result[[type]])
-  addPrefix(tbl, paste0(type, "_"))
+  if(is.null(event[[type]])) {
+    return(NULL)
+  }
+  tbl <- as.data.table(event[[type]])
+  tbl <- addPrefix(tbl, paste0(type, "_"))
   tbl
 }
 
@@ -28,4 +32,44 @@ getRepoTable <- function(event) {
   getTable(event, "repo")
 }
 
-getAllTables <
+getParentTable <- function(event) {
+  data.table(
+    event_id = event$id,
+    event_type = event$type,
+    event_public = event$pubic,
+    event_created_at = event$created_at
+  )
+}
+
+# this needs to change by event type
+getPayloadTable <- function(event) {
+  do.call(
+    paste0("get",
+           event$type,
+           "Payload"),
+    args = list(
+      payload = event$payload
+    )
+  )
+}
+
+
+# parse payload for create event
+getCreateEventPayload <- function(payload) {
+  payload <- as.data.table(payload)
+  payload <- addPrefix(payload, "create_")
+  payload
+}
+
+# return of all the tables from the event
+getAllTables <- function(event) {
+  list(
+    EventTable = getParentTable(event),
+    ActorTable = getActorTable(event),
+    OrgTable = getOrgTable(event),
+    RepoTable = getRepoTable(event),
+    PayloadTable = getPayloadTable(event)
+  )
+}
+
+
